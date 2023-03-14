6c6f83cf8d6a29dd32f26bd089a6d1e008f4e1ee
Anastasia Storozhenko <anastasiastorozhenko@Anastasias-MacBook-Air.local>
2023-03-14 14:46:42 +0200

    added ui input

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
-<script setup>
-
-</script>
-
 <template>
-  <div></div>
+
 </template>
+
+<script setup>
+</script>
---
+++ src/components/UI/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <input 
+    :id="id" 
+    :type="type" 
+    :name="name" 
+    :placeholder="placeholder" 
+  />
+</template>
+
+<script setup>
+
+defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'text'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    default: 'Write here...'
+  }
+});
+</script>
