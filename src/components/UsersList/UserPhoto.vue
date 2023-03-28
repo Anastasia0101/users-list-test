de2d52bdf1f2259e352cda3b34601cc189d63fb3
Anastasia <fairy.tail010101@gmail.com>
2023-03-28 11:25:24 +0300

    added user's photo to list item

---
+++ src/components/UsersList/UserPhoto.vue
@@ -0,0 +1,18 @@
+<template>
+  <Image :url="photoUrl" :altText="altText" />
+</template>
+
+<script setup>
+import Image from './../UI/Image.vue';
+
+defineProps({
+  photoUrl: {
+    type: String,
+    required: true
+  },
+  altText: {
+    type: String,
+    required: true
+  }
+});
+</script>
--- src/components/UsersList/UsersListItem.vue
+++ src/components/UsersList/UsersListItem.vue
@@ -1,8 +1,10 @@
 <template>
-  <p>{{ user.name }}</p>
+  <UserPhoto :photo-url="user.photo" alt-text="'User Photo'" />
 </template>
 
 <script setup>
+import UserPhoto from './UserPhoto.vue';
+
 defineProps({
   user: {
     type: Object,
