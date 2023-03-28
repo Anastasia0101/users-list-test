0ded41545b725b5d5660578f26c7058bcc32e1e9
Anastasia <fairy.tail010101@gmail.com>
2023-03-28 18:47:45 +0300

    added component subtitle

---
+++ src/components/SubTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <Text class="subtitle">
+    What defines a good front-end developer is one that has skilled 
+    knowledge of HTML, CSS, JS with a vast understanding of User design 
+    thinking as they'll be building web interfaces with accessibility in mind. 
+    They should also be excited to learn, as the world of Front-End Development 
+    keeps evolving.
+  </Text>
+</template>
+
+<script setup>
+import Text from './UI/Text.vue';
+</script>
+
+<style lang="sass" scoped>
+@import './../styles/__main'
+
+.subtitle
+  @include base-text
+  text-align: center
+  color: #FFFFFF
+</style>
