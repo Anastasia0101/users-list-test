91f20ce1241512dd72067318a8287e5cc7014c5e
Anastasia <fairy.tail010101@gmail.com>
2023-03-28 13:26:45 +0300

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+
+const Page = () => import('./../views/Page.vue');
+const SignUpForm = () => import('./../components/SignUpForm/SignUpForm.vue');
+const UsersList = () => import('./../components/UsersList/UsersList.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [ ]
+  routes: [ 
+    {
+      path: '',
+      component: Page,
+      children: [
+        {
+          path: 'users',
+          component: UsersList
+        },
+        {
+          path: 'sign-up',
+          component: SignUpForm
+        }
+      ]
+    }
+   ]
 })
 
 export default router
