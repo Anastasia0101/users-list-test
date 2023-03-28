69c1ebd9e6eb5fc98c0a6dd08076ad38a909c969
Anastasia <fairy.tail010101@gmail.com>
2023-03-28 11:20:59 +0300

    added users list item

--- src/components/UsersList/UsersList.vue
+++ src/components/UsersList/UsersList.vue
@@ -1,8 +1,14 @@
 <template>
-
+  <ul>
+    <li v-for="user in users" :key="user.id">
+      <UsersListItem :user="user" />
+    </li>
+  </ul>
 </template>
 
 <script setup>
+import UsersListItem from './UsersListItem.vue';
+
 const url = 'https://frontend-test-assignment-api.abz.agency/api/v1/users?';
 const parameters = `page=${1}&count=${6}`;
 
---
+++ src/components/UsersList/UsersListItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>{{ user.name }}</p>
+</template>
+
+<script setup>
+defineProps({
+  user: {
+    type: Object,
+    required: true
+  }
+});
+</script>
