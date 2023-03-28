acc244bebc58d5504b7fef400b4a27298dac3266
Anastasia <fairy.tail010101@gmail.com>
2023-03-28 11:34:45 +0300

    added user's information to list item

---
+++ src/components/UsersList/UserText.vue
@@ -0,0 +1,15 @@
+<template>
+  <Text>
+    {{ text }}
+  </Text>
+</template>
+
+<script setup>
+import Text from './../UI/Text.vue';
+defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+});
+</script>
--- src/components/UsersList/UsersListItem.vue
+++ src/components/UsersList/UsersListItem.vue
@@ -1,9 +1,14 @@
 <template>
   <UserPhoto :photo-url="user.photo" alt-text="'User Photo'" />
+  <UserText :text="user.name" />
+  <UserText :text="user.position" />
+  <UserText :text="user.email" />
+  <UserText :text="user.phone" />
 </template>
 
 <script setup>
 import UserPhoto from './UserPhoto.vue';
+import UserText from './UserText.vue';
 
 defineProps({
   user: {
