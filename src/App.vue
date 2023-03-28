962f1cc2b7a85d0f089251e68425d4ed2fa9d3fe
Anastasia <fairy.tail010101@gmail.com>
2023-03-28 11:03:51 +0300

    got list of users from api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
+  <Suspense>
+      <template #default>
+        <UsersList />
+      </template>
+      <template #fallback>
+        Loading...
+      </template>
+    </Suspense>
   <SignUpForm />
 </template>
 
 <script setup>
+import UsersList from './components/UsersList/UsersList.vue';
 import SignUpForm from './components/SignUpForm/SignUpForm.vue';
 </script>
---
+++ src/components/UsersList/UsersList.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script setup>
+const url = 'https://frontend-test-assignment-api.abz.agency/api/v1/users?';
+const parameters = `page=${1}&count=${6}`;
+
+const users = await fetch(url + parameters)
+  .then(response => response.json())
+  .then(result => result.users);
+</script>
